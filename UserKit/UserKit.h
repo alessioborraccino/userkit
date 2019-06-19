08211d386a32e79c879b7e58225b8db7b4583451
Alessio Borraccino <alessio.borraccino@gmail.com>
2019-06-19 10:24:41 +0200

    Initial Commit

---
+++ UserKit/UserKit.h
@@ -0,0 +1,19 @@
+//
+//  UserKit.h
+//  UserKit
+//
+//  Created by Alessio Borraccino on 19.06.19.
+//  Copyright © 2019 Alessio Borraccino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UserKit.
+FOUNDATION_EXPORT double UserKitVersionNumber;
+
+//! Project version string for UserKit.
+FOUNDATION_EXPORT const unsigned char UserKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserKit/PublicHeader.h>
+
+
